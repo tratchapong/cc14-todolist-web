f16f19d35806b798f5b60e9a7fefb177280cbf70
tratchapong <tratchapong@gmail.com>
2023-05-15 00:55:09 +0700

    login + register form with Font-Awesome

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,22 @@
-
-
 function Login() {
   return (
-    <div>Login</div>
-  )
+    <div className="the-form w-full max-w-[800px] mx-auto my-6 p-5 border">
+      <form>
+        <h2 className="mb-5 text-xl">Login Form</h2>
+        <div className="input-container">
+          <i className="fa fa-user icon" />
+          <input className="input-field" type="text" placeholder="Username" name="usrnm" />
+        </div>
+        <div className="input-container">
+          <i className="fa fa-key icon" />
+          <input className="input-field" type="password" placeholder="Password" name="psw" />
+        </div>
+        <button type="submit" className="btn">
+          Login
+        </button>
+      </form>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -2,6 +2,29 @@
 
 export default function Register() {
   return (
-    <div>Register</div>
+    <div className="the-form w-full max-w-[800px] mx-auto my-6 p-5 border">
+    <form>
+      <h2 className="mb-5 text-xl">Register Form</h2>
+      <div className="input-container">
+        <i className="fa fa-user icon" />
+        <input className="input-field" type="text" placeholder="Username" name="usrnm" />
+      </div>
+      {/* <div className="input-container">
+        <i className="fa fa-envelope icon" />
+        <input className="input-field" type="text" placeholder="Email" name="email" />
+      </div> */}
+      <div className="input-container">
+        <i className="fa fa-key icon" />
+        <input className="input-field" type="password" placeholder="Password" name="psw" />
+      </div>
+      <div className="input-container">
+        <i className="fa fa-key icon" />
+        <input className="input-field" type="password" placeholder="Confirm password" name="cf_psw" />
+      </div>
+      <button type="submit" className="btn">
+        Register
+      </button>
+    </form>
+  </div>
   )
 }
