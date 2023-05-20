cd99c26e60f1a104e6c7e4bc4da99997c39ffab8
tratchapong <tratchapong@gmail.com>
2023-05-20 14:14:34 +0700

    Register : done

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,7 +1,31 @@
+import {useState} from 'react'
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
 export default function Register() {
+  const navigate = useNavigate()
+  const [input, setInput] = useState({
+    username: '',
+    password: '',
+    confirmPassword: ''
+  })
+  const hdlChangeInput = (e) => {
+    setInput({...input, [e.target.name] : e.target.value })
+  }
+
+  const hdlSubmit = e => {
+    e.preventDefault()
+    if(input.password !== input.confirmPassword)
+      return alert('Please check Password & Confirm Password')
+    axios.post('http://localhost:8080/auth/register', input)
+    .then( rs => {
+      console.log(rs)
+      navigate('/login')
+    })
+  }
+
   return (
     <div className="the-form w-full max-w-[800px] mx-auto my-6 p-5 border">
-      <form>
+      <form onSubmit={hdlSubmit}>
         <h2 className="mb-5 text-xl">Register Form </h2>
         <div className="flex w-full mb-[15px]">
           <i className="fa fa-user text-white w-12 text-center p-2.5 bg-blue-500" />
@@ -9,7 +33,9 @@ export default function Register() {
             className="w-full p-2.5 border focus:bg-blue-100"
             type="text"
             placeholder="Username"
-            name="usrnm"
+            name="username"
+            value={input.username}
+            onChange={hdlChangeInput}
           />
         </div>
         <div className="flex w-full mb-[15px]">
@@ -18,7 +44,9 @@ export default function Register() {
             className="w-full p-2.5 border focus:bg-blue-100"
             type="password"
             placeholder="Password"
-            name="psw"
+            name="password"
+            value={input.password}
+            onChange={hdlChangeInput}
           />
         </div>
         <div className="flex w-full mb-[15px]">
@@ -27,7 +55,9 @@ export default function Register() {
             className="w-full p-2.5 border focus:bg-blue-100"
             type="password"
             placeholder="Confirm password"
-            name="cf_psw"
+            name="confirmPassword"
+            value={input.confirmPassword}
+            onChange={hdlChangeInput}
           />
         </div>
         <button
