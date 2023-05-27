099218461a884aab4520ff896e62b7a326786da0
tratchapong <tratchapong@gmail.com>
2023-05-27 17:08:38 +0700

    use Joi & react-hook-form in Register & Login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,58 +1,59 @@
 /* eslint-disable no-unused-vars */
-import {useState} from 'react'
+import {useForm} from 'react-hook-form'
+import Joi from 'joi';
+import { joiResolver } from '@hookform/resolvers/joi';
 import {login, getMe} from '../api/todoApi'
 import { useNavigate } from 'react-router-dom';
 import useAuth  from "../hooks/useAuth";
 
+const LoginSchema = Joi.object({
+  username : Joi.string().alphanum().min(3).required(),
+  password : Joi.string().min(3).required(), 
+})
+
 function Login() {
+  const {register, handleSubmit, formState: {errors}} = useForm({
+    resolver: joiResolver(LoginSchema)
+  })
   const navigate = useNavigate()
   const {user, setUser} = useAuth()
-  const [input, setInput] = useState({
-    username: '',
-    password: '',
-  })
-  const hdlChangeInput = (e) => {
-    setInput({...input, [e.target.name] : e.target.value })
-  }
 
-  const hdlSubmit = e => {
-    e.preventDefault()
-    login(input)
+  const onSubmit = formData => {
+    login(formData)
     .then( rs => {
       localStorage.setItem('token', rs.data.token)
       return getMe(rs.data.token)
     }).then( rs => {
       setUser(rs.data)
       navigate('/')
-    }).catch( err => console.log(err))
+    }).catch( err => alert(err.response?.data.error || err.message))
   }
 
   return (
     <div className="w-full max-w-[800px] mx-auto my-6 p-5 border">
-      <form onSubmit={hdlSubmit}>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <h2 className="mb-5 text-xl">Login Form</h2>
-        <div className="flex w-full mb-[15px]">
+        <div className="flex w-full mb-4">
           <i className="fa fa-user text-white min-w-[50px] text-center p-2.5 bg-blue-500" />
           <input
             className="w-full p-2.5 border focus:bg-blue-100"
             type="text"
             placeholder="Username"
-            name="username"
-            value={input.username}
-            onChange={hdlChangeInput}
+            {...register('username')}
           />
         </div>
-        <div className="flex w-full mb-[15px]">
+        { errors.username && <p className='text-red-500 mb-4 mt-[-12px]'>{errors.username.message}</p>}
+
+        <div className="flex w-full mb-4">
           <i className="fa fa-key  text-white min-w-[50px] text-center p-2.5 bg-blue-500" />
           <input
             className="w-full p-2.5 border focus:bg-blue-100"
             type="password"
             placeholder="Password"
-            name="password"
-            value={input.password}
-            onChange={hdlChangeInput}
+            {...register('password')}
           />
         </div>
+        { errors.password && <p className='text-red-500 mb-4 mt-[-12px]'>{errors.password.message}</p>}
         <button
           type="submit"
           className="bg-blue-500 text-white cursor-pointer w-full opacity-90 px-5 py-4 my-5 border-none hover:opacity-100"
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,66 +1,69 @@
-import {useState} from 'react'
-import {register} from '../api/todoApi'
+/* eslint-disable no-unused-vars */
+import {useForm} from 'react-hook-form'
+import { joiResolver } from '@hookform/resolvers/joi'
+import { register as registerApi} from '../api/todoApi'
 import { useNavigate } from 'react-router-dom'
+import Joi from  'joi'
+
+
+const RegisterSchema = Joi.object({
+  username : Joi.string().alphanum().min(3).required(),
+  password : Joi.string().min(3).required(),
+  confirmPassword : Joi.ref('password')
+})
 
 export default function Register() {
   const navigate = useNavigate()
-  const [input, setInput] = useState({
-    username: '',
-    password: '',
-    confirmPassword: ''
+  const {register, handleSubmit, formState: { errors }} = useForm({
+    resolver: joiResolver(RegisterSchema)
   })
-  const hdlChangeInput = (e) => {
-    setInput({...input, [e.target.name] : e.target.value })
-  }
 
-  const hdlSubmit = e => {
-    e.preventDefault()
-    if(input.password !== input.confirmPassword)
-      return alert('Please check Password & Confirm Password')
-    register(input)
+  const onSubmit = formData => {
+    registerApi(formData)
     .then( rs => {
       console.log(rs.data)
       navigate('/login')
-    }).catch(err => console.log(err.response?.data || err.message))
+    }).catch(err => console.log(err.response?.data.error || err.message))
+  }
+
+  const onSubmit1 = formData => {
+    console.log(formData)
   }
 
   return (
     <div className="the-form w-full max-w-[800px] mx-auto my-6 p-5 border">
-      <form onSubmit={hdlSubmit}>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <h2 className="mb-5 text-xl">Register Form </h2>
-        <div className="flex w-full mb-[15px]">
+        <div className="flex w-full mb-4">
           <i className="fa fa-user text-white w-12 text-center p-2.5 bg-blue-500" />
           <input
             className="w-full p-2.5 border focus:bg-blue-100"
             type="text"
-            placeholder="Username"
-            name="username"
-            value={input.username}
-            onChange={hdlChangeInput}
+            placeholder='Username'
+            {...register('username')}
           />
         </div>
+          { errors.username && <p className='text-red-500 mb-4 mt-[-12px]'>{errors.username.message}</p>}
         <div className="flex w-full mb-[15px]">
           <i className="fa fa-key  text-white w-12 text-center p-2.5 bg-blue-500" />
           <input
             className="w-full p-2.5 border focus:bg-blue-100"
             type="password"
             placeholder="Password"
-            name="password"
-            value={input.password}
-            onChange={hdlChangeInput}
+            {...register('password')}
           />
         </div>
+          { errors.password && <p className='text-red-500 mb-4 mt-[-12px]'>{errors.password.message}</p>}
         <div className="flex w-full mb-[15px]">
           <i className="fa fa-key  text-white w-12 text-center p-2.5 bg-blue-500" />
           <input
             className="w-full p-2.5 border focus:bg-blue-100"
             type="password"
             placeholder="Confirm password"
-            name="confirmPassword"
-            value={input.confirmPassword}
-            onChange={hdlChangeInput}
+            {...register('confirmPassword')}
           />
         </div>
+          { errors.confirmPassword && <p className='text-red-500 mb-4 mt-[-12px]'>{`Confirm Password didn't match`}</p>}
         <button
           type="submit"
           className="bg-blue-500 text-white cursor-pointer w-full opacity-90 px-5 py-4 my-5 border-none hover:opacity-100"
